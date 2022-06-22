cb4fe2a8d3ab54fd9f13839b7d1e853c28f98bbb
Konstantin <konstantinderendyaev7@mail.com>
2022-06-22 20:52:43 +0500

    Задачи на функции

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const lines = 5;
-let result = '';
-
-for (let i = 0; i <= lines; i++) {
-	for (let j = 0; j < lines - i; j++) {
-		result += ' ';
-	}
-	for (let j = 0; j < 2 * i + 1; j++) {
-		result += '*';
-	}
-	result += '\n';
+function returnNeighboringNumbers(num) {
+	return [num - 1, num, num + 1];
 }
-console.log (result);
+console.log(returnNeighboringNumbers(7));
+
+function sayHello(name) {
+	return `Hello, ${name}!`;
+}
+
+function getMathResult(num, times) {
+	if (typeof(times) !== 'number' || times <= 0) {
+		return num;
+	}
+	
+	let str = '';
+	
+	for (let i = 1; i <= times; i++) {
+		if(i === times) {
+			str += `${num * i}`;
+		} else {
+			str += `${num * i}---`;
+		}
+	}
+	return str;
+}
+console.log(getMathResult(3, 9));
